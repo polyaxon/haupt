962f0204701b047918ab38ecf0ada59afe92aa2b
polyaxon-ci <ci@polyaxon.com>
2022-11-12 18:25:42 +0100

    Fix streams command

--- haupt/haupt/cli/streams.py
+++ haupt/haupt/cli/streams.py
@@ -11,7 +11,7 @@ import click
 
 def start_streams(host: str, port: int, workers: int, per_core: bool, uds: str):
     """Start streams service."""
-    from haupt.cli.runners.sandbox import start
+    from haupt.cli.runners.streams import start
     from polyaxon.env_vars.keys import EV_KEYS_PROXY_STREAMS_TARGET_PORT
 
     port = port or os.environ.get(EV_KEYS_PROXY_STREAMS_TARGET_PORT)
