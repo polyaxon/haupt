c9c9c751d9ea83cbd1af9f18a7ac01f51e62cc37
polyaxon-ci <ci@polyaxon.com>
2023-05-02 22:27:50 +0200

    Bump version to v2.0.0-rc13

--- haupt/haupt/pkg.py
+++ haupt/haupt/pkg.py
@@ -6,7 +6,7 @@
 # LICENSE-AGPL for a copy of the license.
 
 NAME = "haupt"
-VERSION = "2.0.0-rc12"
+VERSION = "2.0.0-rc13"
 DESC = (
     "Lineage metadata API, artifacts streams, sandbox, ML-API, and spaces for Polyaxon."
 )
