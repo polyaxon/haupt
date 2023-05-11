7d8b70834574cde4da6c81596e20211c001dc348
mouradmourafiq <mouradmourafiq@gmail.com>
2023-05-11 22:13:03 +0200

    Bump version to v2.0.0-rc14

--- haupt/haupt/pkg.py
+++ haupt/haupt/pkg.py
@@ -6,7 +6,7 @@
 # LICENSE-AGPL for a copy of the license.
 
 NAME = "haupt"
-VERSION = "2.0.0-rc13"
+VERSION = "2.0.0-rc14"
 DESC = (
     "Lineage metadata API, artifacts streams, sandbox, ML-API, and spaces for Polyaxon."
 )
