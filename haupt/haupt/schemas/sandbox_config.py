139e220f7063b9dd3365297b0f0f5853488bd25d
polyaxon-ci <ci@polyaxon.com>
2023-05-23 23:09:04 +0100

    Inherent artifacts store logic from base agent

--- haupt/haupt/schemas/sandbox_config.py
+++ haupt/haupt/schemas/sandbox_config.py
@@ -3,7 +3,6 @@ from typing import Optional
 from clipped.utils.http import clean_host
 from pydantic import Field, StrictInt, StrictStr
 
-from polyaxon.connections import V1Connection, V1ConnectionKind, V1HostPathConnection
 from polyaxon.env_vars.keys import (
     EV_KEYS_K8S_NAMESPACE,
     EV_KEYS_SANDBOX_DEBUG,
@@ -40,13 +39,3 @@ class SandboxConfig(AgentConfig):
             host=clean_host(host) if host else host,
             **data,
         )
-
-    def set_default_artifacts_store(self):
-        if not self.artifacts_store:
-            self.artifacts_store = V1Connection(
-                name="local",
-                kind=V1ConnectionKind.HOST_PATH,
-                schema_=V1HostPathConnection(
-                    host_path=self.store_root, mount_path=self.store_root
-                ),
-            )
