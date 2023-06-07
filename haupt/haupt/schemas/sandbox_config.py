19c3c480704977422b90fad672117457c46d37d6
polyaxon-ci <ci@polyaxon.com>
2023-06-07 23:14:18 +0200

    Add support for camel case for sandbox config files

--- haupt/haupt/schemas/sandbox_config.py
+++ haupt/haupt/schemas/sandbox_config.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from clipped.utils.http import clean_host
-from pydantic import Field, StrictInt, StrictStr
+from pydantic import Field, StrictInt, StrictStr, root_validator
 
 from polyaxon.env_vars.keys import (
     EV_KEYS_K8S_NAMESPACE,
@@ -39,3 +39,25 @@ class SandboxConfig(AgentConfig):
             host=clean_host(host) if host else host,
             **data,
         )
+
+    @root_validator(pre=True)
+    def handle_camel_case_sandbox(cls, values):
+        if (
+            not values.get("ssl_enabled")
+            and not values.get(EV_KEYS_SANDBOX_SSL_ENABLED)
+            and "sslEnabled" in values
+        ):
+            values[EV_KEYS_SANDBOX_SSL_ENABLED] = values["sslEnabled"]
+        if (
+            not values.get("per_core")
+            and not values.get(EV_KEYS_SANDBOX_PER_CORE)
+            and "perCore" in values
+        ):
+            values[EV_KEYS_SANDBOX_PER_CORE] = values["perCore"]
+        if (
+            not values.get("is_local")
+            and not values.get(EV_KEYS_SANDBOX_IS_LOCAL)
+            and "isLocal" in values
+        ):
+            values[EV_KEYS_SANDBOX_IS_LOCAL] = values["isLocal"]
+        return values
