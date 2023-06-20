1261e4feceb94be682bb32d67b6efa3e7d93d781
polyaxon-ci <ci@polyaxon.com>
2023-06-20 12:06:57 +0200

    Improve manage command CLI

--- haupt/haupt/cli/runners/manage.py
+++ haupt/haupt/cli/runners/manage.py
@@ -4,11 +4,12 @@ from typing import List, Optional
 
 from haupt import settings
 from haupt.cli.runners.base import manage
-from polyaxon.env_vars.keys import EV_KEYS_SERVICE
+from polyaxon.env_vars.keys import EV_KEYS_SERVICE, EV_KEYS_UI_IN_SANDBOX
 from polyaxon.services.values import PolyaxonServices
 
 
 def run_manage(command: str, args: Optional[List[str]] = None):
     os.environ[EV_KEYS_SERVICE] = PolyaxonServices.API
+    os.environ[EV_KEYS_UI_IN_SANDBOX] = "true"
     settings.set_sandbox_config()
     manage(command, args)
