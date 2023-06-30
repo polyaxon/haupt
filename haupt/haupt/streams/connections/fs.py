f0a83253d3ed0e182e3a68f08aaadced7aa4dd1a
polyaxon-ci <ci@polyaxon.com>
2023-06-30 16:57:55 +0200

    Fix regression with timestamp field with pyarrow backend

--- haupt/haupt/streams/connections/fs.py
+++ haupt/haupt/streams/connections/fs.py
@@ -1,8 +1,11 @@
+import os
+
 from typing import Optional
 
 from polyaxon.connections import CONNECTION_CONFIG
 from polyaxon.contexts import paths as ctx_paths
 from polyaxon.env_vars.getters import get_artifacts_store_name
+from polyaxon.env_vars.keys import EV_KEYS_SANDBOX_IS_LOCAL
 from polyaxon.fs.fs import close_fs, get_async_fs_from_connection, get_default_fs
 from polyaxon.fs.types import FSSystem
 
@@ -33,7 +36,8 @@ class AppFS:
 
     @classmethod
     async def get_fs(cls, connection: Optional[str] = None) -> FSSystem:
-        connection = connection or get_artifacts_store_name()
+        if not connection or os.environ.get(EV_KEYS_SANDBOX_IS_LOCAL):
+            connection = get_artifacts_store_name()
         fs = cls._connections.get(connection)
         if not fs:
             return await cls.set_fs(connection=connection)
