0a8375b521f4395f3a904af8153c240b11cf34d6
polyaxon-ci <ci@polyaxon.com>
2023-08-30 17:36:17 +0200

    Add missing project resources serializers tests

--- haupt/tests/test_apis/test_serializers/test_project_resources_serializers.py
+++ haupt/tests/test_apis/test_serializers/test_project_resources_serializers.py
@@ -1,16 +1,68 @@
 import pytest
 
-from haupt.apis.serializers.runs import (
-    OfflineRunSerializer,
-    OperationCreateSerializer,
-    RunSerializer,
+from haupt.apis.serializers.project_versions import (
+    ProjectVersionDetailSerializer,
+    ProjectVersionSerializer,
 )
-from haupt.common import conf
-from haupt.common.options.registry.k8s import K8S_NAMESPACE
-from haupt.db.factories.runs import RunFactory
-from haupt.db.models.runs import Run
-from polyaxon.lifecycle import V1Statuses
-from polyaxon.polyflow import V1CloningKind
-from tests.base.case import BaseTestRunSerializer
+from tests.base.case import BaseTestProjectVersionSerializer
 
-del BaseTestRunSerializer
+
+@pytest.mark.serializers_mark
+class TestProjectVersionSerializer(BaseTestProjectVersionSerializer):
+    serializer_class = ProjectVersionSerializer
+    expected_keys = {
+        "uuid",
+        "name",
+        "description",
+        "tags",
+        "created_at",
+        "updated_at",
+        "stage",
+        "state",
+        "kind",
+    }
+
+    def test_serialize_one(self):
+        obj1 = self.create_one()
+        data = self.serializer_class(obj1).data
+
+        assert set(data.keys()) == self.expected_keys
+        data.pop("created_at")
+        data.pop("updated_at")
+        assert data.pop("uuid") == obj1.uuid.hex
+
+        for k, v in data.items():
+            assert getattr(obj1, k) == v
+
+
+@pytest.mark.serializers_mark
+class TestProjectVersionDetailSerializer(TestProjectVersionSerializer):
+    serializer_class = ProjectVersionDetailSerializer
+    expected_keys = TestProjectVersionSerializer.expected_keys | {
+        "owner",
+        "project",
+        "content",
+        "readme",
+        "run",
+        "artifacts",
+        "meta_info",
+    }
+
+    def test_serialize_one(self):
+        obj1 = self.create_one()
+        data = self.serializer_class(obj1).data
+
+        assert set(data.keys()) == self.expected_keys
+        data.pop("created_at")
+        data.pop("updated_at")
+        assert data.pop("uuid") == obj1.uuid.hex
+        assert data.pop("owner") == obj1.project.owner.name
+        assert data.pop("project") == obj1.project.name
+        assert data.pop("artifacts") == []
+        assert data.pop("meta_info") == {}
+
+        for k, v in data.items():
+            assert getattr(obj1, k) == v
+
+
+del BaseTestProjectVersionSerializer
