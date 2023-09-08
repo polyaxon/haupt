420c9949b2f69a2c2667e1a4080f1c54833f8313
polyaxon-ci <ci@polyaxon.com>
2023-09-08 13:34:05 +0200

    Update cron schedule

--- haupt/haupt/cli/runners/cron.py
+++ haupt/haupt/cli/runners/cron.py
@@ -4,7 +4,7 @@ import threading
 import time
 
 from polyaxon.client import PolyaxonClient
-from polyaxon.constants.globals import NO_AUTH
+from polyaxon.constants.globals import DEFAULT, NO_AUTH
 from polyaxon.schemas.cli.client_config import ClientConfig
 from polyaxon.sdk.exceptions import ApiException
 from urllib3.exceptions import HTTPError
@@ -18,12 +18,12 @@ def agent_cron(host, stop_event: threading.Event):
     time.sleep(5)
     while not stop_event.is_set():
         try:
-            polyaxon_client.agents_v1.cron_agent(_request_timeout=10)
+            polyaxon_client.agents_v1.cron_agent(owner=DEFAULT, _request_timeout=10)
         except (ApiException, HTTPError) as e:
             _logger.debug(e)
             _logger.info("Waiting for Polyaxon to be ready...")
-            time.sleep(3)
-        time.sleep(1)
+            time.sleep(5)
+        time.sleep(2)
 
 
 def start_cron(host: str):
