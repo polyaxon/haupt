4195350fcd1dcd0d5988b01cb16490277f334703
polyaxon-ci <ci@polyaxon.com>
2024-10-22 11:28:56 +0200

    Improve handling of large query params

--- haupt/tests/test_proxies/test_api/test_base.py
+++ haupt/tests/test_proxies/test_api/test_base.py
@@ -29,6 +29,11 @@ charset utf-8;
 client_max_body_size        0;
 client_body_buffer_size     50m;
 client_body_in_file_only clean;
+large_client_header_buffers 8 1m;
+client_header_buffer_size 1m;
+uwsgi_buffer_size 1m;
+uwsgi_buffers 8 1m;
+uwsgi_busy_buffers_size 2m;
 sendfile on;
 
 
@@ -160,6 +165,11 @@ charset utf-8;
 client_max_body_size        0;
 client_body_buffer_size     50m;
 client_body_in_file_only clean;
+large_client_header_buffers 8 1m;
+client_header_buffer_size 1m;
+uwsgi_buffer_size 1m;
+uwsgi_buffers 8 1m;
+uwsgi_busy_buffers_size 2m;
 sendfile on;
 
 
