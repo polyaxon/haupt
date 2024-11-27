867e29b0863fd68df14a1d4d388f8e16564227bf
polyaxon-ci <ci@polyaxon.com>
2024-11-27 14:58:15 +0100

    Add admin view for artifacts lineage

--- haupt/haupt/db/administration/artifacts.py
+++ haupt/haupt/db/administration/artifacts.py
@@ -14,3 +14,18 @@ class ArtifactAdmin(DiffModelAdmin):
     )
     readonly_fields = DiffModelAdmin.readonly_fields + ("name",)
     fields = ("name", "kind", "created_at", "updated_at")
+
+
+class ArtifactLineageAdmin(DiffModelAdmin):
+    list_display = (
+        "artifact",
+        "run",
+        "is_input",
+    )
+    list_display_links = (
+        "artifact",
+        "run",
+        "is_input",
+    )
+    readonly_fields = DiffModelAdmin.readonly_fields + ("artifact", "run")
+    fields = ("artifact", "run", "is_input", "created_at", "updated_at")
--- haupt/haupt/db/administration/register.py
+++ haupt/haupt/db/administration/register.py
@@ -1,12 +1,13 @@
 from django.contrib.admin import site
 from django.contrib.auth.admin import UserAdmin
 
-from haupt.db.administration.artifacts import ArtifactAdmin
+from haupt.db.administration.artifacts import ArtifactAdmin, ArtifactLineageAdmin
 from haupt.db.administration.projects import ProjectAdmin
 from haupt.db.administration.runs import RunLightAdmin
 from haupt.db.defs import Models
 
 site.register(Models.User, UserAdmin)
 site.register(Models.Artifact, ArtifactAdmin)
+site.register(Models.ArtifactLineage, ArtifactLineageAdmin)
 site.register(Models.Project, ProjectAdmin)
 site.register(Models.Run, RunLightAdmin)
